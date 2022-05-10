83025897983b3dd2e053ef5ec8a86231d85c2a8c
davidphanva <david.phan.va@gmail.com>
2022-05-10 12:48:56 -0400

    Added defaults for Ford Bronco.

--- src/main/java/org/example/usage/BroncoDefault.java
+++ src/main/java/org/example/usage/BroncoDefault.java
@@ -1,4 +1,12 @@
 package org.example.usage;
 
 public class BroncoDefault extends FordDefault {
+
+    @Override
+    public void init(Ford ford) {
+        super.init(ford);
+
+        ford.horsePower = 300;
+        ford.modelName = "Bronco";
+    }
 }
